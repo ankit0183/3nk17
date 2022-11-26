e310b8bf150db4c0781271c2c109ee10e9c7d2f1
Ankít Kanœjíya <ankitkanojiya624@gmail.com>
2022-11-26 22:09:10 +0530

    update config yml

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221126.225156';
+const cacheName = 'chirpy-20221127.003646';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
