24236199e5ca0833422799baa1d4b3de5994a708
a3nk17 <a3nk17@localhost.localdomain>
2023-02-10 02:18:38 +0530

    new exploit added

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221127.144814';
+const cacheName = 'chirpy-20230210.044717';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
